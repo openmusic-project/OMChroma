3ba4fc193acef6d40221954a2d4afcd7cc570355
j-bresson <bresson@ircam.fr>
2018-05-14 12:29:45 +0200

    fix (?) conflict with 'chord class: cr must not "use" OM

--- sources/package.lisp
+++ sources/package.lisp
@@ -1,7 +1,9 @@
 
 
 (defpackage "CHROMA" 
-  (:use "COMMON-LISP" "OM")
+  (:use "COMMON-LISP")
   (:nicknames "CR"))
+
+;;; :chroma must not :use :om because it redefines the class CHORD
 
 (use-package :chroma :om)
